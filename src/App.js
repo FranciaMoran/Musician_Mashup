1ec613fd2a2dd15f4e9f9b44030102dd059423c4
Francia Moran <franciam115@gmail.com>
2018-08-16 13:50:31 -0500

    second page added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import Landing from './components/landingPage'
 import './App.css';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Login from './components/loginPage'
 
 export default function App(props) { 
     return (
       <main>
       <Route exact path="/" component={Landing}/>
+      <Route exact path="/Login" component={Login}/>
       </main>
     );
 }
--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -7,14 +7,19 @@ export default class Landing extends React.Component {
     constructor(props) {
         super(props)
     }
-    
+    goToLogin(event) {
+        event.preventDefault();
+        this.props.history.push(`/Login`);
+        alert("hey");
+    }
+
     render() {
         return (
             <div className="landing-page">
                 <h1 id="name-of-app">Musician Mashup</h1>
                 <h2 id="slogan">Get in contact with musicians to form a band today!</h2>
                     <p id="app-explained">Musician Mashup is a networking app which allows musicians to meet new people to jam with. As a user, you can create a personal profile and add members to a new band or you can join existing groups.</p>
-                    <button id="get-started-button">Get Started!</button>
+                    <button id="get-started-button" onClick={e => this.goToLogin(e)}>Get Started!</button>
                 <h3 id="demo-login">Demo Login</h3>
                     <p id="demo-login-description">You can use this login to see an example of a profile and the home dashboard.</p>
                     <ul>
