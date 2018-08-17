182600a24f541e7c23008f12a5716d17c288b3a0
Francia Moran <franciam115@gmail.com>
2018-08-17 13:18:50 -0500

    login page react set

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -2,17 +2,37 @@ import React from 'react';
 
 import './loginPage.css';
 
-export default class Login extends React.Component {
+import {login} from '../actions/auth';
+
+import {Field, reduxForm, focus} from 'redux-form';
+
+export class Login extends React.Component {
+
     goToCreateAccount(event) {
         event.preventDefault();
         this.props.history.push(`/createAccount`);
     }
 
+    onSubmit(values) {
+        return this.props.dispatch(login(values.username, values.password));
+    }
+
     render() {
+        let error;
+        if (this.props.error) {
+            error = (
+                <div className="form-error" aria-live="polite">
+                    {this.props.error}
+                </div>
+            );
+        }
         return (
             <div>
             <h1 id="login-title">Login</h1>
-            <form id="login-inputs">
+            <form className="login-inputs" 
+            onSubmit={this.props.handleSubmit(values =>
+                    this.onSubmit(values)
+                )}>
     <div id="username-input">
     <label>Username:</label>
     <input type="text"></input>
@@ -28,3 +48,9 @@ export default class Login extends React.Component {
         );
     }
 }
+
+export default reduxForm({
+    form: 'login',
+    onSubmitFail: (errors, dispatch) => dispatch(focus('login', 'username'))
+})(Login);
+
