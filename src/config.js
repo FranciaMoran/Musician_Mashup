ce7aae1e6c0edf41991d7f134f9f29b7bbb8afe6
Francia Moran <franciam115@gmail.com>
2018-09-19 10:09:57 -0500

    connecting 1

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'https://stark-harbor-96755.herokuapp.com';
+    process.env.REACT_APP_API_BASE_URL || 'https://stark-harbor-96755.herokuapp.com/api';
